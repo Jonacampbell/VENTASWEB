6a04c94b1aeb9c7b500d1d96de952f72fa3d151a
fernando <fdabrego@gmail.com>
2022-07-28 19:06:26 -0300

    Cambios Look&feel

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -8,6 +8,6 @@ namespace CapaDatos
 {
     public class Conexion
     {
-        public static string CN = "Data Source=.;Initial Catalog=DBPRUEBAS;Integrated Security=True";
+        public static string CN = "Data Source=LAPTOP-DO3EHHQ6\\SQLEXPRESS;Initial Catalog=DBPRUEBAS;Integrated Security=True";
     }
 }
